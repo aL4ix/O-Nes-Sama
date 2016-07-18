966563ba27cf4d348aa71a14099a4e603d56f719
fuumarumota <you@example.com>
2016-07-17 23:14:50 -0500

    Controller Input Class Cleanup

--- Input.cpp
+++ Input.cpp
@@ -1,49 +1,25 @@
 #include "Input.h"
 
-Input::Input(){
-    strobeHigh = false;
-    shftRegValP1 = 0;
-    shftRegValP2 = 0;
-}
+Input::Input(){}
 
 void Input::latchButtonStatus(){
     const uint8_t* state = SDL_GetKeyboardState(NULL);
-    /* Player one */
-    shftRegValP1 = 0;
-    shftRegValP1 = shftRegValP1 | (state[SDL_SCANCODE_S]);
-    shftRegValP1 = shftRegValP1 | (state[SDL_SCANCODE_A] << 1);
-    shftRegValP1 = shftRegValP1 | (state[SDL_SCANCODE_BACKSPACE] << 2);
-    shftRegValP1 = shftRegValP1 | (state[SDL_SCANCODE_RETURN] << 3);
-    shftRegValP1 = shftRegValP1 | (state[SDL_SCANCODE_UP] << 4);
-    if(!state[SDL_SCANCODE_UP])
-        shftRegValP1 = shftRegValP1 | (state[SDL_SCANCODE_DOWN] << 5);
-    shftRegValP1 = shftRegValP1 | (state[SDL_SCANCODE_LEFT] << 6);
-    if(!state[SDL_SCANCODE_LEFT])
-        shftRegValP1 = shftRegValP1 | (state[SDL_SCANCODE_RIGHT] << 7);
-
-    /* Player two */
-    shftRegValP2 = 0;
-    shftRegValP2 = shftRegValP2 | (state[SDL_SCANCODE_W]);
-    shftRegValP2 = shftRegValP2 | (state[SDL_SCANCODE_Q] << 1);
-    shftRegValP2 = shftRegValP2 | (state[SDL_SCANCODE_SPACE] << 2);
-    shftRegValP2 = shftRegValP2 | (state[SDL_SCANCODE_L] << 3);
-    shftRegValP2 = shftRegValP2 | (state[SDL_SCANCODE_U] << 4);
-    shftRegValP2 = shftRegValP2 | (state[SDL_SCANCODE_J] << 5);
-    shftRegValP2 = shftRegValP2 | (state[SDL_SCANCODE_H] << 6);
-    shftRegValP2 = shftRegValP2 | (state[SDL_SCANCODE_K] << 7);
+    shftRegs[0] = 0;
+    shftRegs[1] = 0;
+    for (int i=0; i<8; i++){
+        if ((shftRegs[0] & _Up_Dn) == _Up_Dn)     shftRegs[0] &= ~ _Up; //Don't allow Up + Down
+        if ((shftRegs[0] & _Lft_Rgt) == _Lft_Rgt) shftRegs[0] &= ~ _Lft; //Don't allow Left + Right;
+        if ((shftRegs[1] & _Up_Dn) == _Up_Dn)     shftRegs[1] &= ~ _Up;
+        if ((shftRegs[1] & _Lft_Rgt) == _Lft_Rgt) shftRegs[1] &= ~ _Lft;
+        shftRegs[0] = shftRegs[0] | (state[P1Buttons[i]] << i);
+        shftRegs[1] = shftRegs[1] | (state[P2Buttons[i]] << i);
+    }
 }
 
 unsigned char Input::read(int addr){
-    unsigned char shftRegBit = 0;
-
-    if (addr == 0x4016){
-        shftRegBit = shftRegValP1 & 0x1;
-        shftRegValP1 >>= 1;
-    }
-    if (addr == 0x4017){
-        shftRegBit = shftRegValP2 & 0x1;
-        shftRegValP2 >>= 1;
-    }
+    int shftRegBit = 0;
+    shftRegBit =  shftRegs[addr & 1] & 0x1;
+    shftRegs[addr & 1] >>= 1;
     return (shftRegBit | ((addr & 0xE000) >> 8));
 }
 
--- Input.h
+++ Input.h
@@ -2,15 +2,6 @@
 #define INPUT_H_INCLUDED
 
 #include <SDL2/SDL.h>
-//#include "Memory.h"
-const unsigned char _A = 0x01;
-const unsigned char _B = 0x02;
-const unsigned char _Sel = 0x04;
-const unsigned char _Sta = 0x08;
-const unsigned char _Up = 0x10;
-const unsigned char _Dn = 0x20;
-const unsigned char _Lft = 0x40;
-const unsigned char _Rgt = 0x80;
 
 class Input{
 
@@ -18,15 +9,26 @@ class Input{
         Input();
         unsigned char read(int addr);
         void write (int addr, unsigned char val);
-
-    private:
-
-        int lastWrite;
-        //Memory &memory;
         bool strobeHigh;
-        unsigned char shftRegValP1, shftRegValP2;
         void latchButtonStatus();
-        void latchP1Buttons();
+    private:
+        const unsigned char _A       = 0x01;
+        const unsigned char _B       = 0x02;
+        const unsigned char _Sel     = 0x04;
+        const unsigned char _Sta     = 0x08;
+        const unsigned char _Up      = 0x10;
+        const unsigned char _Dn      = 0x20;
+        const unsigned char _Lft     = 0x40;
+        const unsigned char _Rgt     = 0x80;
+        const unsigned char _Up_Dn   = 0x30;
+        const unsigned char _Lft_Rgt = 0xC0;
+        //Initialize P1 & P2 Controllers, eventually this will be done from the GUI
+        unsigned char P1Buttons[8] = { SDL_SCANCODE_S , SDL_SCANCODE_A   , SDL_SCANCODE_BACKSPACE, SDL_SCANCODE_RETURN,
+                                       SDL_SCANCODE_UP, SDL_SCANCODE_DOWN, SDL_SCANCODE_LEFT     , SDL_SCANCODE_RIGHT };
+
+        unsigned char P2Buttons[8] = { SDL_SCANCODE_W , SDL_SCANCODE_Q   , SDL_SCANCODE_SPACE    , SDL_SCANCODE_L,
+                                       SDL_SCANCODE_U , SDL_SCANCODE_J   , SDL_SCANCODE_H        , SDL_SCANCODE_K };
+        unsigned char shftRegs[2];
 };
 
 #endif // INPUT_H_INCLUDED
