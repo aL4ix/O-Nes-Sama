490138af4ec9f4b547a2df10201be7af2980debb
fuumarumota <you@example.com>
2016-07-18 00:12:42 -0500

    Controller Input Small Enhancements

--- Input.cpp
+++ Input.cpp
@@ -4,21 +4,18 @@ Input::Input(){}
 
 void Input::latchButtonStatus(){
     const uint8_t* state = SDL_GetKeyboardState(NULL);
-    shftRegs[0] = 0;
-    shftRegs[1] = 0;
-    for (int i=0; i<8; i++){
-        if ((shftRegs[0] & _Up_Dn) == _Up_Dn)     shftRegs[0] &= ~ _Up; //Don't allow Up + Down
-        if ((shftRegs[0] & _Lft_Rgt) == _Lft_Rgt) shftRegs[0] &= ~ _Lft; //Don't allow Left + Right;
-        if ((shftRegs[1] & _Up_Dn) == _Up_Dn)     shftRegs[1] &= ~ _Up;
-        if ((shftRegs[1] & _Lft_Rgt) == _Lft_Rgt) shftRegs[1] &= ~ _Lft;
-        shftRegs[0] = shftRegs[0] | (state[P1Buttons[i]] << i);
-        shftRegs[1] = shftRegs[1] | (state[P2Buttons[i]] << i);
+
+    for (int j=0; j<2; j++){
+        for (int i=0; i<8; i++){
+            shftRegs[j] = shftRegs[j] | (state[Buttons[j][i]] << i);
+            if ((shftRegs[j] & _Up_Dn) == _Up_Dn)     shftRegs[j] &= ~ _Up; //Don't allow Up + Down
+            if ((shftRegs[j] & _Lft_Rgt) == _Lft_Rgt) shftRegs[j] &= ~ _Lft; //Don't allow Left + Right;
+        }
     }
 }
 
 unsigned char Input::read(int addr){
-    int shftRegBit = 0;
-    shftRegBit =  shftRegs[addr & 1] & 0x1;
+    int shftRegBit =  shftRegs[addr & 1] & 0x1;
     shftRegs[addr & 1] >>= 1;
     return (shftRegBit | ((addr & 0xE000) >> 8));
 }
--- Input.h
+++ Input.h
@@ -23,11 +23,17 @@ class Input{
         const unsigned char _Up_Dn   = 0x30;
         const unsigned char _Lft_Rgt = 0xC0;
         //Initialize P1 & P2 Controllers, eventually this will be done from the GUI
-        unsigned char P1Buttons[8] = { SDL_SCANCODE_S , SDL_SCANCODE_A   , SDL_SCANCODE_BACKSPACE, SDL_SCANCODE_RETURN,
-                                       SDL_SCANCODE_UP, SDL_SCANCODE_DOWN, SDL_SCANCODE_LEFT     , SDL_SCANCODE_RIGHT };
+        unsigned char Buttons[2][8] = {
+            {  //Player 1 Buttons
+                SDL_SCANCODE_S , SDL_SCANCODE_A   , SDL_SCANCODE_BACKSPACE, SDL_SCANCODE_RETURN,
+                SDL_SCANCODE_UP, SDL_SCANCODE_DOWN, SDL_SCANCODE_LEFT     , SDL_SCANCODE_RIGHT
+            },
+            {  //Player 2 Buttons
+                SDL_SCANCODE_W , SDL_SCANCODE_Q   , SDL_SCANCODE_SPACE    , SDL_SCANCODE_L,
+                SDL_SCANCODE_U , SDL_SCANCODE_J   , SDL_SCANCODE_H        , SDL_SCANCODE_K
+            }
+        };
 
-        unsigned char P2Buttons[8] = { SDL_SCANCODE_W , SDL_SCANCODE_Q   , SDL_SCANCODE_SPACE    , SDL_SCANCODE_L,
-                                       SDL_SCANCODE_U , SDL_SCANCODE_J   , SDL_SCANCODE_H        , SDL_SCANCODE_K };
         unsigned char shftRegs[2];
 };
 
